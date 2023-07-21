3341c627fc0cfacf1c6c761ec0e3a92f9dfd5185
givisab <givisab12345@gmail.com>
2023-07-21 23:56:45 +0400

    Edit javascript, reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function reverse(word) {
+let result = "";
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        result += word[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
