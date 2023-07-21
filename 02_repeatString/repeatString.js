8b44588b9cad8992eab17531e86e878632c2677a
givisab <givisab12345@gmail.com>
2023-07-21 21:10:13 +0400

    Edit javascript, repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const num = prompt("Provide a number");
+const word = prompt("Provide a word");
 
+const repeatString = function repeat(word, num) {
+    let result = "";
+
+    if (num < 0) {
+        result += "ERROR";
+    }
+
+    for (let i = 0; i < num; i++) {
+        result += word;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
