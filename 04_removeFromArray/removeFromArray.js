7740a71bb976f0aee4fd48bb9c7d62570e89ab9f
givisab <givisab12345@gmail.com>
2023-07-23 14:02:49 +0400

    Edit JS, remove arguments from arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function remove(array, ...args)  {
 
+    /* Creating new array, where we are going to store 
+    value, that should not be removed from an array
+    */
+
+    let newArray = [];
+
+    // Looping through array elements
+    for (index in array) {
+
+        /* Checking whether array contains arguments
+        passed as ...args, in order to create new array,
+        with only the value, that should not have
+        been removed from an original array
+        */
+       
+        if (args.includes(array[index]) === false) {
+            newArray.push(array[index]);
+        }
+    };
+    return newArray;
 };
 
 // Do not edit below this line
