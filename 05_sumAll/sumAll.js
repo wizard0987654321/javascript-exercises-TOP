79d232388a5a3ee017d4cf20f4005852dedca422
givisab <givisab12345@gmail.com>
2023-07-23 14:26:47 +0400

    Edit js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function sum(firstNum, secondNum) {
+    let result = 0;
+    // Checking, whether both arguments are positive integers
+    if (secondNum < 0 || firstNum < 0)  {
+        return ('ERROR');
+    // Checking, whether types of both arguments are numbers
+    } else if (typeof(firstNum) != "number" || 
+        typeof(secondNum) != "number") {
+        return ('ERROR');
+    // Checking, which argument has higher value and looping according to that
+    } else if (secondNum < firstNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            result += i;
+        }
+    }
+    
+    for (let i = firstNum; i <= secondNum; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
