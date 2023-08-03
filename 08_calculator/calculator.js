716a0eb8b42f36f4b9575d4ddb082e8076a17fc4
givisab <givisab12345@gmail.com>
2023-08-03 15:40:44 +0400

    create calculator, use array reduce function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+  for (let i = 0; i < b; i++) {
+    result = result * a;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+
+  if (number === 0) {
+    return 1;
+  }
+  for (let i = number; i > 0; i--) {
+    result = result * i;
+  }
+  
+  return result;
 };
 
 // Do not edit below this line
