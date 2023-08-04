9ff3a7c868a12dff43f038299e36229969e74bc1
givisab <givisab12345@gmail.com>
2023-08-04 23:46:49 +0400

    Edit js, get the objects from array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let length = array.length;
+    let titles = [];
 
+    for (let i = 0; i < length; i++) {
+        let titleToAdd = array[i]["title"]
+        titles.push(titleToAdd);
+    }
+    return titles;
 };
 
 // Do not edit below this line
