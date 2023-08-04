05ed1bd39ea407c445247a344c9ebf18c0294b33
givisab <givisab12345@gmail.com>
2023-08-04 22:41:56 +0400

    Edit js, create fibonacci array pattern

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let fibonacciNums = [1, 1];
+    let i = 0;
+    let position = Number(number)
 
+    if (position < 0) {
+        return "OOPS";
+    }else if (position === 0) {
+        return 0;
+    }else if (position === 1) {
+        return 1;
+    }else if (position === 2) {
+        return 1;
+    };
+
+    while (fibonacciNums.length <= number ) {
+        let numToAdd = 0;
+        numToAdd += fibonacciNums[i] + fibonacciNums[i + 1];
+        fibonacciNums.push(numToAdd);
+        i++;
+    }
+
+    return fibonacciNums[i];
 };
 
 // Do not edit below this line
