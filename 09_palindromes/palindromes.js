f079563beadff9574a572dc60d2ca4c09fa20a24
givisab <givisab12345@gmail.com>
2023-08-04 21:19:51 +0400

    Edit js, check, whether words are palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let example = word.toUpperCase();
+    example = example.replace(/[^\w\s]/g, '');
+    example = example.replace(/\s/g, "");
+    let midpoint = 0;
+    let length = example.length;
+    
 
+    if (length % 2 === 0) {
+        midpoint += (length / 2);
+
+        let first = example.substring(0, midpoint);
+        let second = example.substring(midpoint);
+        second = second.split('').reverse().join('');
+        return first === second ? true:
+        false;
+    }
+        midpoint += Math.floor(length / 2);
+
+        let first = example.substring(0, midpoint);
+        let second = example.substring(midpoint + 1);
+        second = second.split('').reverse().join('');
+        return first === second ? true:
+        false;
 };
 
 // Do not edit below this line
