99b50758572892896ec881539dc00d93cd40a060
givisab <givisab12345@gmail.com>
2023-08-05 23:51:28 +0400

    Edit js, find the oldest person in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let oldest = array[0];
 
+    for (let i = 1; i < array.length; i++) {
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        }
+
+        if (array[i].yearOfDeath - array[i].yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+            oldest = array[i];
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
